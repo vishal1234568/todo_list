055cec954ead8626ade7c23854e181caa5505405
vishal1234568 <vishalrathod00143@gmail.com>
2022-05-27 21:49:50 +0530

    Todo List

---
+++ db/seeds.rb
@@ -0,0 +1,22 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+todos = Todo.create([
+  {title: "Buy Groceries", description: "Go to Safeway, don't forget the coupons."},
+
+  {title: "Wash the Car", description: "Have a date this Saturday."},
+
+  {title: "Clean my room", description: "It's that time of year again."},
+
+  {title: "Do the Laundry", description: "Don't forget the bleach!"},
+
+  {title: "Work on Mini-Project", description: "What are Happy Tails anyway?"},
+
+  {title: "Walk the Dog", description: "Take Fido around the block."}
+
+   ])
+   
